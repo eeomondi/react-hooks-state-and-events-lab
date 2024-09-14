78a3880705dd858bfe97d748149ee2347087e7fd
Elvis <elvis.omondi@student.moringaschool.com>
2024-09-14 17:22:47 +0300

    commit

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // State variable to manage item cart status
+  const [inCart, setInCart] = useState(false);
+
+  // Toggle function
+  const toggleCart = () => {
+    setInCart(prevInCart => !prevInCart);
+  };
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={toggleCart}>
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
 
 export default Item;
+
